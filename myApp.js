56d19a1ccd120b9bc9c993ebeca175268511dda3
EricoBis <ericobis.santos@gmail.com>
2024-01-22 22:37:25 +0000

    add: middleware function

--- myApp.js
+++ myApp.js
@@ -3,15 +3,24 @@ let app = express();
 
 console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(function middleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/", function (req, res) {
-  const absolutePath = __dirname + '/views/index.html';
+  const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
-app.get("/json", function(req, res){
-  res.json({"message": "Hello json"});
-})
-
-app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function (req, res) {
+  res.json(
+    process.env.MESSAGE_STYLE == "uppercase"
+      ? { message: "HELLO JSON" }
+      : { message: "Hello json" },
+  );
+});
 
 module.exports = app;
