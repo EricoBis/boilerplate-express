f5f162413019167e96161f2b7737d86323e31929
EricoBis <ericobis.santos@gmail.com>
2024-01-22 21:29:56 +0000

    add: basic routes to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,17 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", function (req, res) {
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
+});
 
+app.get("/json", function(req, res){
+  res.json({"message": "Hello json"});
+})
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
